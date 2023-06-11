82b51bb6e83318964d5532b2f40c85f91b6a76be
isaias-silva <zackblackmangold@gmail.com>
2023-06-11 17:46:59 -0300

    comentários

--- teste6.js
+++ teste6.js
@@ -1,6 +1,6 @@
 /**
  * @comentario : middleware criado para barrar acesso,
- *  o middleware checa se a propriedade acessclass nos header da requisição
+ *  o middleware checa se a propriedade @acessclass nos header da requisição
  * tem o valor 'admin' caso contrário ele nega o acesso.
  * */
  
